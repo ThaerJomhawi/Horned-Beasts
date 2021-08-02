8fa141e9f3f40f93e171de3194813789fbd7697c
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-02 23:21:59 +0300

    02Lab

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,16 +1,31 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card'
 class HornedBeast extends React.Component {
+  state = {
+    stateVotes: 0
+  }
+
+  raiseVotes = () => {
+    this.setState({
+      stateVotes: this.state.stateVotes + 1
+    })
+
+  }
   render() {
-    const { title, imageUrl, description} = this.props;
+    const { title, imageUrl, description } = this.props;
     return (
-
-      <div>
-        <h2>{title}</h2>
-        <img src={imageUrl} alt={title} />
-        <p>{description}</p>
-      </div>
-
+      <Card className="mb-5" style={{ width: '18rem' }}>
+        <Card.Img className="card-img" variant="top" src={imageUrl} alt={title} onClick={() => { this.raiseVotes() }} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {description}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          {this.state.stateVotes}
+        </Card.Footer>
+      </Card>
     );
   }
 }
